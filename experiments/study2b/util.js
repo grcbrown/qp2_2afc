544dc9ea868d6edbbd97dba7ade164c273929a13
grcbrown <122645861+grcbrown@users.noreply.github.com>
2025-11-25 13:43:41 -0800

    subfolders for each study

---
+++ experiments/study2b/util.js
@@ -0,0 +1,12 @@
+function create_tv_array(json_object) {
+    let tv_array = [];
+    for (let i = 0; i < json_object.length; i++) {
+        obj = {};
+        obj.stimulus = json_object[i].stimulus;
+        obj.id = json_object[i].id;
+        obj.speaker = json_object[i].speaker;
+        obj.data = {};
+        tv_array.push(obj)
+    }
+    return tv_array;
+}
